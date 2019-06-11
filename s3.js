29ce595667862173689c464bb7a9c673005883c5
Natalia Zakrevskaya <zakrevsn@gmail.com>
2019-06-12 00:29:43 +0200

    new bucket

--- s3.js
+++ s3.js
@@ -1,17 +1,17 @@
-const knox = require('knox-s3');
-const fs = require('fs');
+const knox = require("knox-s3");
+const fs = require("fs");
 
 let secrets;
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV == "production") {
     secrets = process.env; // in prod the secrets are environment variables
 } else {
-    secrets = require('./secrets'); // secrets.json is in .gitignore
+    secrets = require("./secrets"); // secrets.json is in .gitignore
 }
 
 const client = knox.createClient({
     key: secrets.AWS_KEY,
     secret: secrets.AWS_SECRET,
-    bucket: 'spicedling'
+    bucket: "rumandmonkey"
 });
 
 exports.upload = function(req, res, next) {
@@ -19,14 +19,14 @@ exports.upload = function(req, res, next) {
         return res.sendStatus(500);
     }
     const s3Request = client.put(req.file.filename, {
-        'Content-Type': req.file.mimetype,
-        'Content-Length': req.file.size,
-        'x-amz-acl': 'public-read'
+        "Content-Type": req.file.mimetype,
+        "Content-Length": req.file.size,
+        "x-amz-acl": "public-read"
     });
     const stream = fs.createReadStream(req.file.path);
     stream.pipe(s3Request);
 
-    s3Request.on('response', s3Response => {
+    s3Request.on("response", s3Response => {
         console.log(s3Response.statusCode, req.file.filename);
         if (s3Response.statusCode == 200) {
             next();
